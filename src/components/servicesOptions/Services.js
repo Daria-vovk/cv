a34dafa0fe4faa5f728199a2d1a44714b914ff0c
Yevhen <skaretskiy1999@gmail.com>
2022-11-13 16:13:02 +0200

    Second try

--- src/components/servicesOptions/Services.js
+++ src/components/servicesOptions/Services.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import classNames from "classnames";
 import { v4 as uuidv4 } from 'uuid';
-import { useTransition, animated, config } from "react-spring";
+import { useTransition, animated } from "react-spring";
 
 import { Button, SliderService } from "../transponder";
 
